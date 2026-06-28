a2b3755a4a36c4253369e431923cb1f5c3701141
JosephAv <g.averta3@gmail.com>
2026-06-28 14:03:16 +0200

    first commit

---
+++ database/DAO.py
@@ -0,0 +1,28 @@
+from database.DB_connect import DBConnect
+
+
+class DAO():
+
+    @staticmethod
+    def getAllCountries():
+        conn = DBConnect.get_connection()
+
+        results = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """
+                select distinct c.Country
+                from customer c
+                where c.Country is not null
+                order by c.Country
+                """
+
+        cursor.execute(query)
+
+        for row in cursor:
+            results.append(row["Country"])
+
+        cursor.close()
+        conn.close()
+        return results
+
